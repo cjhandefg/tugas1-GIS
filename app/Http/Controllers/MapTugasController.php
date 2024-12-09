26dffa25f89a3ba508b4752367b9f5b281d01d89
cjhandefg <89524146+cjhandefg@users.noreply.github.com>
2024-12-09 10:58:36 +0800

    Revert "Update MapTugasController.php"
    This reverts commit ce5bafd563e683079fb2e4f348a0b1439e9af2da.

--- app/Http/Controllers/MapTugasController.php
+++ app/Http/Controllers/MapTugasController.php
@@ -6,14 +6,8 @@ use Illuminate\Http\Request;
 
 class MapTugasController extends Controller
 {
-  public function saveAsHtml()
-  {
-      // Render the Blade view as HTML
-      $html = view('tugas1')->render();
-
-      // Save it as a static HTML file in the public folder
-      File::put(public_path('tugas1.html'), $html);
-
-      return 'HTML file generated successfully!';
-  }
+    public function tugas1()
+    {
+        return view('tugas1');
+    }
 }
